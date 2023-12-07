b72b70903461e1f5b0724673ded2a605fef2167a
Janiel Flores Almanzar <janielfloresdo@gmail.com>
2023-12-06 17:20:18 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System.Threading;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace SpotifyAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EdgeOptions options = new EdgeOptions();
+            IWebDriver driver = new EdgeDriver(options);
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl("https://www.spotify.com");
+
+            // Definir rutas absolutas para las capturas y el informe
+            string capturasPath = @"C:\Users\janie\source\repos\PruebasAutomatizadas\Capturas";
+            string reportePath = @"C:\Users\janie\source\repos\PruebasAutomatizadas\Reporteinforme_pruebas.html";
+
+            // Configurar el reporte
+            var htmlReporter = new ExtentHtmlReporter(reportePath);
+            var extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+
+            // Crear un nuevo informe de prueba
+            var test = extent.CreateTest("Prueba automatizada de Spotify", "Ejecución de pruebas automatizadas en Spotify");
+
+            PerformLogin(driver, test, capturasPath);
+            SearchSong(driver, "Belivier", test, capturasPath);
+            PlaySong(driver, test, capturasPath);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            PauseSong(driver, test, capturasPath);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            SkipNextSong(driver, test, capturasPath);
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+            SkipPreviousSong(driver, test, capturasPath);
+            PerformLogout(driver, test, capturasPath);
+
+            // Cerrar el navegador y finalizar el informe
+            driver.Quit();
+            extent.Flush();
+        }
+
+        static void PerformLogin(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                Screenshot screenshotButtonVisible = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotButtonVisiblePath = System.IO.Path.Combine(capturasPath, "screenshotButtonVisible.png");
+                screenshotButtonVisible.SaveAsFile(screenshotButtonVisiblePath, ScreenshotImageFormat.Png);
+
+                IWebElement iniciarSesionButton = driver.FindElement(By.XPath("//button[contains(@data-testid, 'login-button')]"));
+                iniciarSesionButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                Screenshot screenshotFormVisible = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotFormVisiblePath = System.IO.Path.Combine(capturasPath, "screenshotFormVisible.png");
+                screenshotFormVisible.SaveAsFile(screenshotFormVisiblePath, ScreenshotImageFormat.Png);
+
+                IWebElement correoInput = driver.FindElement(By.Id("login-username"));
+
+                correoInput.SendKeys("20220194@itla.edu.do");
+
+                IWebElement contrasenaInput = driver.FindElement(By.Id("login-password"));
+
+                contrasenaInput.SendKeys("1234567");
+
+                IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+                loginButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                Screenshot screenshotLobbyIncorrect = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotLobbyIncorrectPath = System.IO.Path.Combine(capturasPath, "screenshotLobbyIncorrect.png");
+                screenshotLobbyIncorrect.SaveAsFile(screenshotLobbyIncorrectPath, ScreenshotImageFormat.Png);
+
+                contrasenaInput.Clear();
+
+                contrasenaInput.SendKeys("Ingresar64");
+
+                loginButton = driver.FindElement(By.Id("login-button"));
+                loginButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                Screenshot screenshotLobby = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotLobbyPath = System.IO.Path.Combine(capturasPath, "screenshotLobby.png");
+                screenshotLobby.SaveAsFile(screenshotLobbyPath, ScreenshotImageFormat.Png);
+
+                test.Pass("Inicio de sesión realizado correctamente");
+
+            }
+
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+
+        }
+
+        static void PerformLogout(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                Screenshot screenshotIconProfile = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotIconProfilePath = System.IO.Path.Combine(capturasPath, "screenshotIconProfile.png");
+                screenshotIconProfile.SaveAsFile(screenshotIconProfilePath, ScreenshotImageFormat.Png);
+
+                IWebElement menuButton = driver.FindElement(By.CssSelector("button[data-testid='user-widget-link']"));
+                menuButton.Click();
+
+                Screenshot screenshotMenu = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotMenuPath = System.IO.Path.Combine(capturasPath, "screenshotMenu.png");
+                screenshotMenu.SaveAsFile(screenshotMenuPath, ScreenshotImageFormat.Png);
+
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                IWebElement logoutButton = driver.FindElement(By.CssSelector("button[data-testid='user-widget-dropdown-logout']"));
+                logoutButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                Screenshot screenshotLogout = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotLogoutPath = System.IO.Path.Combine(capturasPath, "screenshotLogout.png");
+                screenshotLogout.SaveAsFile(screenshotLogoutPath, ScreenshotImageFormat.Png);
+
+                test.Pass("Cierre de sesión realizado correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+
+        static void SearchSong(IWebDriver driver, string songName, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                Screenshot screenshotSearch = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotSearchPath = System.IO.Path.Combine(capturasPath, "screenshotSearch.png");
+                screenshotSearch.SaveAsFile(screenshotSearchPath, ScreenshotImageFormat.Png);
+
+                IWebElement searchButton = driver.FindElement(By.CssSelector("a[aria-label='Search']"));
+                searchButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                IWebElement searchInput = driver.FindElement(By.CssSelector("input[data-testid='search-input']"));
+
+                searchInput.SendKeys(songName);
+
+                Screenshot screenshotSearchInput = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotSearchInputPath = System.IO.Path.Combine(capturasPath, "screenshotSearchInput.png");
+                screenshotSearchInput.SaveAsFile(screenshotSearchInputPath, ScreenshotImageFormat.Png);
+
+                searchInput.SendKeys(Keys.Enter);
+
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                Screenshot screenshotSearchResult = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotSearchResultPath = System.IO.Path.Combine(capturasPath, "screenshotSearchResult.png");
+                screenshotSearchResult.SaveAsFile(screenshotSearchResultPath, ScreenshotImageFormat.Png);
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+                test.Pass("Búsqueda de canción realizada correctamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+
+        static void PlaySong(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                // Encontrar y hacer clic en el botón de reproducir
+                IWebElement playButton = driver.FindElement(By.CssSelector("button[aria-label='Play'][data-testid='control-button-playpause']"));
+                playButton.Click();
+
+                // Esperar 2 segundos
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+
+                Screenshot screenshotPlay = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotPlayPath = System.IO.Path.Combine(capturasPath, "screenshotPlay.png");
+                screenshotPlay.SaveAsFile(screenshotPlayPath, ScreenshotImageFormat.Png);
+
+                test.Pass("Canción reproducida exitosamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+
+        static void PauseSong(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                // Encontrar y hacer clic en el botón de pausar
+                IWebElement pauseButton = driver.FindElement(By.CssSelector("button[aria-label='Pausar']"));
+                pauseButton.Click();
+
+                Screenshot screenshotPause = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotPausePath = System.IO.Path.Combine(capturasPath, "screenshotPause.png");
+                screenshotPause.SaveAsFile(screenshotPausePath, ScreenshotImageFormat.Png);
+
+                test.Pass("Canción pausada exitosamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+
+        static void SkipNextSong(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                // Encontrar y hacer clic en el botón de siguiente canción
+                IWebElement nextButton = driver.FindElement(By.CssSelector("button[data-testid='control-button-skip-forward']"));
+                nextButton.Click();
+
+                Screenshot screenshotSkipNextSong = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotSkipNextSongPath = System.IO.Path.Combine(capturasPath, "screenshotSkipNextSong.png");
+                screenshotSkipNextSong.SaveAsFile(screenshotSkipNextSongPath, ScreenshotImageFormat.Png);
+
+
+                test.Pass("Siguiente canción reproducida exitosamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+
+        static void SkipPreviousSong(IWebDriver driver, ExtentTest test, string capturasPath)
+        {
+            try
+            {
+                // Encontrar y hacer clic en el botón de canción anterior
+                IWebElement previousButton = driver.FindElement(By.CssSelector("button[data-testid='control-button-skip-back']"));
+                previousButton.Click();
+                previousButton.Click();
+
+                Screenshot screenshotSkipPreviousSong = ((ITakesScreenshot)driver).GetScreenshot();
+                string screenshotSkipPreviousSongPath = System.IO.Path.Combine(capturasPath, "screenshotSkipPreviousSong.png");
+                screenshotSkipPreviousSong.SaveAsFile(screenshotSkipPreviousSongPath, ScreenshotImageFormat.Png);
+
+                test.Pass("Canción anterior reproducida exitosamente");
+
+            }
+            catch (Exception ex)
+            {
+                test.Fail(ex);
+            }
+
+
+        }
+    }
+}
---
+++ Reporte/README.txt
@@ -0,0 +1,62 @@
+Documentacion de uso:
+
+Para realizar la prueba de automatizacion en Spotify
+
+Pasos a seguir al descargar el proyecto:
+
+Modificar las rutas locales de guardado de reportes y capturas.
+Ejecutar el programa.
+
+Documentacion del ultimo reporte realizado:
+
+Reporte de Prueba Automatizada en Spotify
+Fecha de Prueba: 10 de agosto de 2023
+Hora de Inicio: 10:15:39 p. m.
+Hora de Finalización: 10:16:42 p. m.
+Duración Total: 0 horas, 1 minuto, 2 segundos y 543 milisegundos
+
+Resumen de Ejecución:
+Las siguientes pruebas automatizadas fueron ejecutadas en la aplicación de Spotify, evaluando diversas funcionalidades clave. Las pruebas se llevaron a cabo correctamente, verificando la estabilidad y funcionalidad de la aplicación durante las diferentes etapas.
+
+Detalles de la Ejecución:
+
+1. Inicio de Sesión:
+   - Hora: 10:16:03 p. m.
+   - Estado: Inicio de sesión realizado correctamente
+   - Descripción: Se verificó con éxito que el proceso de inicio de sesión en la aplicación de Spotify se completó sin problemas.
+
+2. Búsqueda de Canción:
+   - Hora: 10:16:14 p. m.
+   - Estado: Búsqueda de canción realizada correctamente
+   - Descripción: Se confirmó que la función de búsqueda de canción respondió adecuadamente y arrojó resultados esperados.
+
+3. Reproducción de Canción:
+   - Hora: 10:16:17 p. m.
+   - Estado: Canción reproducida exitosamente
+   - Descripción: Se validó que la reproducción de una canción se efectuó sin dificultades y el audio se escuchó correctamente.
+
+4.Pausa de Canción:
+   - Hora: 10:16:22 p. m.
+   - Estado: Canción pausada exitosamente
+   - Descripción: Se constató que la pausa de la canción se realizó satisfactoriamente y la reproducción se detuvo como se esperaba.
+
+5. Siguiente Canción:
+   - Hora: 10:16:28 p. m.
+   - Estado: Siguiente canción reproducida exitosamente
+   - Descripción: Se verificó que la función para reproducir la siguiente canción funcionó correctamente y el cambio de pista fue exitoso.
+
+6. Canción Anterior:
+   - Hora: 10:16:33 p. m.
+   - Estado: Canción anterior reproducida exitosamente
+   - Descripción: Se comprobó que la función de reproducción de la canción anterior se realizó sin problemas y la canción previa se reprodujo como se esperaba.
+
+7. Cierre de Sesión:
+   - Hora: 10:16:42 p. m.
+   - Estado: Cierre de sesión realizado correctamente
+   - Descripción: Se constató que el proceso de cierre de sesión fue exitoso y la aplicación finalizó la sesión de manera adecuada.
+
+Conclusión:
+La ejecución de las pruebas automatizadas en la aplicación de Spotify demostró un funcionamiento sólido y coherente en las diferentes funcionalidades evaluadas. No se observaron problemas graves durante la prueba, lo que sugiere una buena estabilidad y calidad en la aplicación durante el período de prueba.
+
+
+
